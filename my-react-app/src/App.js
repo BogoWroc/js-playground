429f01c969aed847a033e450adf4b63612995084
Bogumil Zebek <bogowrocpl@gmail.com>
2018-08-10 18:59:44 +0200

    Add css styling

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -25,10 +25,24 @@ class App extends Component {
     };
 
     namedChangedHandler = (event) => {
-
+        this.setState({
+            persons: [
+                {name: "Maximilian", age: 29},
+                {name: event.target.value, age: 45},
+                {name: "Kate", age: 21}
+            ]
+        })
     };
 
     render() {
+        const style = {
+            backgroundColor: 'white',
+            font: 'inherit',
+            border: '1px solid blue',
+            padding: '8px',
+            cursor: 'pointer'
+        };
+
         return (
             <div className="App">
                 <header className="App-header">
@@ -38,14 +52,17 @@ class App extends Component {
                 <p className="App-intro">
                     To get started, edit <code>src/App.js</code> and save to reload.
                 </p>
-                <button onClick={() => this.switchNameHandler("Maximilian")}>Test</button>
+                <button
+                    style={style}
+                    onClick={() => this.switchNameHandler("Maximilian")}>Test</button>
                 <Person
                     name={this.state.persons[0].name}
                     age={this.state.persons[0].age}
                 />
                 <Person name={this.state.persons[1].name}
                         age={this.state.persons[1].age}
-                        click={this.switchNameHandler.bind(this, "Max!")}>Say hello!
+                        click={this.switchNameHandler.bind(this, "Max!")}
+                        changed={this.namedChangedHandler}>Say hello!
                 </Person>
                 <Person name={this.state.persons[2].name}
                         age={this.state.persons[2].age}
