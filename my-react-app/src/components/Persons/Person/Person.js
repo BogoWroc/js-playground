6bafad91da67468cd7bd001f809751052151f6b4
bogumil_zebek <bogumil.zebek@nokia.com>
2018-08-22 10:55:52 +0200

    Convert to stateful component

--- my-react-app/src/components/Persons/Person/Person.js
+++ my-react-app/src/components/Persons/Person/Person.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, {Component} from 'react';
 import classes from './Person.css'
 
-const person = (props) => {
-    return (
-        <div className={classes.Person}>
-            <p onClick={props.click}>I'm a {props.name} and I'm {props.age} years old!</p>
-            <p>{props.children}</p>
-            <input type="text" onChange={props.changed}/>
-        </div>
-    );
-};
+class Person extends Component {
 
-export default person;
+    render() {
+        return (
+            <div className={classes.Person}>
+                <p onClick={this.props.click}>I'm a {this.props.name} and I'm {this.props.age} years old!</p>
+                <p>{this.props.children}</p>
+                <input type="text" onChange={this.props.changed}/>
+            </div>
+        );
+    }
+}
+
+export default Person;
