27d3ed71738a6dbe5b270b3b23251dd8f15fc8d3
Bogumil Zebek <bogowrocpl@gmail.com>
2018-09-27 11:34:13 +0200

    ChessBoard exercise

---
+++ get-programming-with-js/src/exercises/ChessBoard.js
@@ -0,0 +1,68 @@
+/*
+Write a program that creates a string that represents an 8 × 8 grid, using newline characters to separate lines.
+ At each position of the grid there is either a space or a “#” character. The characters should form a chess board.
+*/
+
+export default function createChessBoard(size) {
+    const board = new Array(size);
+    for (let i = 0; i < board.length; i++) {
+        board[i] = createBoardRow(i, size);
+    }
+    return board;
+}
+
+export function createOddChessBoardRow(size) {
+
+    function generateChar(position) {
+        return (isOddRow(position)) ? '#' : ' ';
+    }
+
+    return fillBoardRow(generateChar, size);
+}
+
+export function createAddChessBoardRow(size) {
+
+    function generateChar(position) {
+        return (isOddRow(position)) ? ' ' : '#';
+    }
+
+    return fillBoardRow(generateChar, size);
+}
+
+
+function createBoardRow(position, size) {
+    return (isOddRow(position)) ? createOddChessBoardRow(size) : createAddChessBoardRow(size);
+}
+
+function isOddRow(position) {
+    return position % 2 == 0;
+}
+
+function fillBoardRow(generateChar, size) {
+    let row = "";
+
+    for (let i = 0; i < size; i++) {
+        row += generateChar(i);
+    }
+
+    return row;
+}
+
+function main(size) {
+    const board = createChessBoard(size);
+    for (let i = 0; i < board.length; i++) {
+        console.log(board[i]);
+    }
+}
+
+if (require.main === module) {
+    main(8);
+    console.log("------------------------------------------");
+    main(1);
+    console.log("------------------------------------------");
+    main(2);
+    console.log("------------------------------------------");
+    main(3);
+    console.log("------------------------------------------");
+    main(4);
+}
---
+++ get-programming-with-js/src/exercises/ChessBoard.test.js
@@ -0,0 +1,40 @@
+import createChessBoard, {createAddChessBoardRow, createOddChessBoardRow} from "./ChessBoard";
+import * as chai from 'chai';
+
+describe("ChessBoard",()=>{
+
+
+    it("should return array 8x8",()=>{
+        function verifyThatEachRowHasProperFormat(board) {
+            for (let i = 0; i < board.length; i++) {
+                if(i%2===0) {
+                    chai.expect(board[i]).to.be.equal("# # # # ");
+                } else {
+                    chai.expect(board[i]).to.be.equal(" # # # #");
+                }
+            }
+        }
+
+        let SIZE = 8;
+        const board = createChessBoard(SIZE);
+
+        chai.expect(board.length).to.be.equal(SIZE);
+        verifyThatEachRowHasProperFormat(board);
+    });
+
+    it("should create odd chess board row in N size",() =>{
+        chai.expect(createOddChessBoardRow(0)).to.be.equal('');
+        chai.expect(createOddChessBoardRow(1)).to.be.equal('#');
+        chai.expect(createOddChessBoardRow(2)).to.be.equal('# ');
+        chai.expect(createOddChessBoardRow(3)).to.be.equal('# #');
+        chai.expect(createOddChessBoardRow(8)).to.be.equal('# # # # ');
+    });
+
+    it("should create add chess board row in N size",() =>{
+        chai.expect(createAddChessBoardRow(0)).to.be.equal('');
+        chai.expect(createAddChessBoardRow(1)).to.be.equal(' ');
+        chai.expect(createAddChessBoardRow(2)).to.be.equal(' #');
+        chai.expect(createAddChessBoardRow(3)).to.be.equal(' # ');
+        chai.expect(createAddChessBoardRow(8)).to.be.equal(' # # # #');
+    });
+});
