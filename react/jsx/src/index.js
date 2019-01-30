50f7941d1b3cc1e8631bc2c2f00da03afbf686d8
Bogumil Zebek <bogowrocpl@gmail.com>
2019-01-30 18:38:05 +0100

    Building content with JSX

--- react/jsx/src/index.js
+++ react/jsx/src/index.js
@@ -2,7 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    return <div>Hi, there!</div>
+    const buttonText = 'Click me!';
+    const labelName = 'Enter name:';
+
+    const style = {
+        backgroundColor: 'blue',
+        color: 'white'
+    };
+    return (
+        <div>
+            <label className="label" htmlFor="name">{labelName}</label>
+            <input id="name" type="text"/>
+            <button style={style}>
+                {buttonText}
+            </button>
+        </div>
+    )
 };
 
 ReactDOM.render(
