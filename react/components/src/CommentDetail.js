5edf246b9c8ed1a70802ff1f5d8bb5aee8da1a18
Zebek Bogumil <bogumil.zebek@nokia.com>
2019-01-31 08:42:57 +0100

    Customize component

--- react/components/src/CommentDetail.js
+++ react/components/src/CommentDetail.js
@@ -1,20 +1,19 @@
 import React from 'react';
-import faker from 'faker';
 
-const CommentDetail = ({author}) => {
+const CommentDetail = ({author, timeAgo, avatar, content}) => {
     return (
         <div className="comment">
             <a href="/" className="avatar">
-                <img alt="avatar" src={faker.image.avatar()}/>
+                <img alt="avatar" src={avatar}/>
             </a>
             <div className="content">
                 <a href="/" className="author">
                     {author}
                 </a>
                 <div className="metadata">
-                    <span className="data">Today at 6:00PM</span>
+                    <span className="data">{timeAgo} </span>
                 </div>
-                <div className="text">Nice blog post!</div>
+                <div className="text">{content}</div>
             </div>
         </div>
     );
--- react/components/src/index.js
+++ react/components/src/index.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import faker from 'faker';
 import CommentDetail from './CommentDetail';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <CommentDetail author="Sam"/>
-            <CommentDetail author="Alex"/>
-            <CommentDetail author="Jane"/>
+            <CommentDetail
+                author="Sam"
+                timeAgo="Today at 3:00PM"
+                avatar={faker.image.avatar()}
+                content="Nice blog post!"/>
+            <CommentDetail
+                author="Alex"
+                timeAgo="Today at 6:00PM"
+                avatar={faker.image.avatar()}
+                content="Nice blog post!"/>
+            <CommentDetail
+                author="Jane"
+                timeAgo="Today at 4:00PM"
+                avatar={faker.image.avatar()}
+                content="Nice blog post!"/>
         </div>
     );
 };
