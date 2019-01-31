e8e8fe9cedfbb2aa47b97a0167477608177eda8d
Zebek Bogumil <bogumil.zebek@nokia.com>
2019-01-31 09:02:13 +0100

    Reuse ApprovalCard component

--- react/components/src/index.js
+++ react/components/src/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail';
 import ApprovalCard from './ApprovalCard';
@@ -6,22 +7,27 @@ import ApprovalCard from './ApprovalCard';
 const App = () => {
     return (
         <div className="ui container comments">
-            <ApprovalCard/>
-            <CommentDetail
-                author="Sam"
-                timeAgo="Today at 3:00PM"
-                avatar={faker.image.avatar()}
-                content="Nice blog post!"/>
-            <CommentDetail
-                author="Alex"
-                timeAgo="Today at 6:00PM"
-                avatar={faker.image.avatar()}
-                content="Nice blog post!"/>
-            <CommentDetail
-                author="Jane"
-                timeAgo="Today at 4:00PM"
-                avatar={faker.image.avatar()}
-                content="Nice blog post!"/>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Sam"
+                    timeAgo="Today at 3:00PM"
+                    avatar={faker.image.avatar()}
+                    content="Nice blog post!"/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Alex"
+                    timeAgo="Today at 6:00PM"
+                    avatar={faker.image.avatar()}
+                    content="Nice blog post!"/>
+            </ApprovalCard>
+            <ApprovalCard>
+                <CommentDetail
+                    author="Jane"
+                    timeAgo="Today at 4:00PM"
+                    avatar={faker.image.avatar()}
+                    content="Nice blog post!"/>
+            </ApprovalCard>
         </div>
     );
 };
