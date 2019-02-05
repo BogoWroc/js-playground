79408283244927660a919e836c58e28587ec97b4
Zebek Bogumil <bogumil.zebek@nokia.com>
2019-02-05 08:33:24 +0100

    Add video player logic

--- react/videos/src/components/App.js
+++ react/videos/src/components/App.js
@@ -1,30 +1,52 @@
 import React from 'react';
 import SearchBar from "./SearchBar";
 import VideoList from "./VideoList";
+import VideoDetail from "./VideoDetail";
 import youtube from '../api/YouTube';
 
 
 class App extends React.Component {
     state = {
-      videos: []
+        videos: [],
+        selectedVideo: null
     };
 
     onSubmit = async (value) => {
-        const response = await youtube.get('/search',{
+        const response = await youtube.get('/search', {
             params: {
                 q: value
             }
         });
 
-        this.setState({videos: response.data.items});
+        this.setState({
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
+        });
 
     };
+
+    onVideoSelect = (video) => {
+        this.setState({selectedVideo: video});
+    };
+
+    componentDidMount() {
+        this.onSubmit('buildings');
+    }
 
     render() {
         return (
             <div className="ui container">
                 <SearchBar onSubmit={this.onSubmit}/>
-                <VideoList videos={this.state.videos}/>
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.selectedVideo}/>
+                        </div>
+                        <div className="five wide column">
+                            <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}/>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
