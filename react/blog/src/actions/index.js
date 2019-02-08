2bb48b2395b6da418249bb26a1778b678a16161e
Zebek Bogumil <bogumil.zebek@nokia.com>
2019-02-08 09:27:28 +0100

    Fix problem with multi server requests

--- react/blog/src/actions/index.js
+++ react/blog/src/actions/index.js
@@ -1,17 +1,38 @@
 import _ from 'lodash';
 import jsonPlaceHolder from '../apis/jsonPlaceholder';
 
+export const fetchPostsAndUsers = () => async (dispatch, getState )=>{
+    await dispatch(fetchPosts());
+
+    // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    // userIds.forEach(id => dispatch(fetchUser(id)));
+
+    // Equivalent with above statement
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value();
+};
+
 export const fetchPosts = () => async dispatch => {
-    const response = await jsonPlaceHolder.get('/posts');
-    dispatch({type: 'FETCH_POSTS', payload: response.data})
-};
+        const response = await jsonPlaceHolder.get('/posts');
+        dispatch({type: 'FETCH_POSTS', payload: response.data})
+    };
 
 
-export const fetchUser = (id) => dispatch => {
-    _fetchUser(id, dispatch);
-};
-
-const _fetchUser = _.memoize(async (id, dispatch) => {
+export const fetchUser = (id) => async dispatch => {
     const response = await jsonPlaceHolder.get(`/users/${id}`);
     dispatch({type: 'FETCH_USER', payload: response.data})
-});
+};
+
+// MEMOIZE solution for multi server requests !!!!
+
+// export const fetchUser = (id) => dispatch => {
+//     _fetchUser(id, dispatch);
+// };
+//
+// const _fetchUser = _.memoize(async (id, dispatch) => {
+//     const response = await jsonPlaceHolder.get(`/users/${id}`);
+//     dispatch({type: 'FETCH_USER', payload: response.data})
+// });
