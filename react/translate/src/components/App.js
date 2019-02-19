c2917f1691fe7c74c1c0a0f47d5db4b361812c44
Bogumil Zebek <bogowrocpl@gmail.com>
2019-02-19 18:34:01 +0100

    Add page content with language selectors

--- react/translate/src/components/App.js
+++ react/translate/src/components/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import UserCreate from './UserCreate';
 
-class App extends React.Component{
+class App extends React.Component {
+
+    state = {language: 'english'};
+
+    onLanguageChange = (language) => {
+        this.setState({language});
+    };
+
     render() {
 
         return (
-            <div>App</div>
+            <div className="ui container">
+                <div>
+                    Select a language:
+                    <i className="flag us" onClick={() => this.onLanguageChange('english')}/>
+                    <i className="flag nl" onClick={() => this.onLanguageChange('dutch')}/>
+                </div>
+                <UserCreate/>
+            </div>
         );
     }
 }
