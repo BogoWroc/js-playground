c97cb6b73dc90f01decc093106293668acde3e8b
Zebek Bogumil <bogumil.zebek@nokia.com>
2019-02-27 14:30:03 +0100

    Add enzyme tests and absolute path imports

--- react/testing/src/components/__tests__/App.test.js
+++ react/testing/src/components/__tests__/App.test.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from '../App';
+import {shallow} from 'enzyme';
+import App from 'components/App';
+import CommentBox from 'components/CommentBox';
+import CommentList from 'components/CommentList';
 
 it('shows a comment box', () => {
-    const div = document.createElement('div');
+   const wrapped = shallow(<App />);
+   expect(wrapped.find(CommentBox)).toHaveLength(1);
 
-    ReactDOM.render(<App/>, div);
+});
 
-    expect(div.innerHTML).toContain('Comment box');
-    ReactDOM.unmountComponentAtNode(div);
+
+it('shows a comment list', () => {
+    const wrapped = shallow(<App />);
+    expect(wrapped.find(CommentList)).toHaveLength(1);
+
 });
