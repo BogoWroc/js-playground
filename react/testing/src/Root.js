674ddffbf402ab9ae33a0fd22708eea7fb125340
Zebek Bogumil <bogumil.zebek@nokia.com>
2019-03-08 12:02:25 +0100

    Add fetch comments logic

--- react/testing/src/Root.js
+++ react/testing/src/Root.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import {Provider} from 'react-redux';
-import {createStore} from 'redux';
+import {applyMiddleware, createStore} from 'redux';
+import reduxPromise from 'redux-promise';
 import reducers from 'reducers';
 
 
-export default ({children, initialState={}}) => {
+export default ({children, initialState = {}}) => {
+
+    const store = createStore(reducers, initialState, applyMiddleware(reduxPromise));
     return (
-        <Provider store={createStore(reducers, initialState)}>
+        <Provider store={store}>
             {children}
         </Provider>
     );
